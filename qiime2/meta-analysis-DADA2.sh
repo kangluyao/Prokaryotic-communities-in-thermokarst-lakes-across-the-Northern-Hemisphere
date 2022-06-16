a47d2d27401cdadfdb6f9a7bf9202b1cd32f357b
kangluyao <luyaokang@outlook.com>
2022-06-16 17:28:45 +0800

    Update meta-analysis-DADA2.sh

--- qiime2/meta-analysis-DADA2.sh
+++ qiime2/meta-analysis-DADA2.sh
@@ -82,14 +82,14 @@ cd ..
 #import sequence and taxon file
 qiime tools import \
 --type 'FeatureData[Sequence]' \
---input-path Silva/SILVA_132_QIIME_release/rep_set/rep_set_16S_only/99/silva_132_99_16S.fna \
+--input-path Silva/SILVA_138_QIIME_release/rep_set/rep_set_16S_only/99/silva_138_99_16S.fna \
 --output-path 99-meta-data/ref_99_seqs_16S.qza
 
 
 qiime tools import \
 --type 'FeatureData[Taxonomy]' \
 --input-format HeaderlessTSVTaxonomyFormat \
---input-path Silva/SILVA_132_QIIME_release/taxonomy/16S_only/99/majority_taxonomy_7_levels.txt \
+--input-path Silva/SILVA_138_QIIME_release/taxonomy/16S_only/99/majority_taxonomy_7_levels.txt \
 --output-path 99-meta-data/ref_99_otus_16S_taxonomy.qza
 
 #Extract reference reads
